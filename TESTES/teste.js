abdea04210992a25dc5dbedfc9714a7483505e63
Eryka Gonçalves de Souza <eryka.souza@unitri.edu.br>
2023-06-07 16:45:47 -0300

    formatação com js

---
+++ TESTES/teste.js
@@ -0,0 +1,9 @@
+function calcularMultiplicacao() {
+    const numero1 = 0.20;
+    const numero2 = 0.20;
+    const resultado = numero1 * numero2;
+    const resultadoFormatado = resultado.toFixed(2);
+    return resultadoFormatado;
+  }
+  
+  console.log(calcularMultiplicacao());
--- home.js
+++ home.js
@@ -1,63 +1,40 @@
-// fetch('https://fakestoreapi.com/products')
-//   .then(res => res.json())
-//   .then(json => {
-//     const apiDataElement = document.getElementById('apiData');
-//     apiDataElement.style.display = 'flex';
-//     apiDataElement.style.margin = '0';
+fetch('https://fakestoreapi.com/products/')
+  .then(res => res.json())
+  .then(json => {
+    const apiDataElement = document.getElementById('apiData');
 
-    
+    // Limpar o conteúdo existente no elemento
+    apiDataElement.innerHTML = '';
 
+    json.forEach(product => {
+      const productContainer = document.createElement('li');
+      productContainer.classList.add('produto-1');
 
-//     // Limpar o conteúdo existente no elemento
-//     apiDataElement.innerHTML = '';
+      const productImageElement = document.createElement('img');
+      productImageElement.src = product.image;
+      productImageElement.alt = 'description image';
+      productImageElement.classList.add('config-img');
+      productContainer.appendChild(productImageElement);
 
-//     json.forEach(product => {
-//       const productContainer = document.createElement('div');
-//       productContainer.classList.add('product-container');
-//       productContainer.style.color = '#191970';
-//       productContainer.style.padding = '50px';
-      
-//       const productImageElement = document.createElement('img');
-//       productImageElement.src = product.image;
-//       productImageElement.alt = 'Product Image';
+      const figcaptionElement = document.createElement('figcaption');
+      figcaptionElement.classList.add('info-produto');
 
-//       productImageElement.style.marginTop = '10px';
-//       productContainer.appendChild(productImageElement);
+      const productTitleElement = document.createElement('h2');
+      productTitleElement.id = 'productTitle';
+      productTitleElement.innerText = product.title;
+      figcaptionElement.appendChild(productTitleElement);
 
-      
-//       const productTitleElement = document.createElement('h2');
-//       productTitleElement.innerText = product.title;
-//       productTitleElement.style.textAlign = 'justify'
-//       productTitleElement.style.letterSpacing = '1px'
-//       productTitleElement.style.fontSize = '20px'
-//       productTitleElement.style.textTransform = 'uppercase'
-//       productTitleElement.style.marginBottom = '10px'
-//       productTitleElement.style.textIndent = '10px'
-//       productTitleElement.style.fontWeight = 'bold'
-//       productTitleElement.style.width = '300px';
-//       productTitleElement.style.height = 'px';
-//       productContainer.appendChild(productTitleElement);
-      
-//       const productPriceElement = document.createElement('p');
-//       productPriceElement.innerText = `Price: $${product.price}`;
-//       productContainer.appendChild(productPriceElement);
-      
-//       const productRatingElement = document.createElement('p');
-//       productRatingElement.innerText = `Rating: ${product.rating.rate} (Count: ${product.rating.count})`;
-//       productContainer.appendChild(productRatingElement);
-//       apiDataElement.appendChild(productContainer);
+      const buttonSalesElement = document.createElement('div');
+      buttonSalesElement.classList.add('button-sales');
 
+      const buttonDetailsElement = document.createElement('button');
+      buttonDetailsElement.classList.add('button-details');
+      buttonDetailsElement.innerText = 'Detalhes';
+      buttonSalesElement.appendChild(buttonDetailsElement);
 
-//       placeholder: 'blur'
-//       productImageElement.style.filter = 'blur(3px)';
-//       productImageElement.style.transition = 'all 0.5s ease-in-out';
-//       productImageElement.onmouseover = function() {
-//         productImageElement.style.filter = 'blur(0px)';
-//       }
-//       productImageElement.onmouseout = function() {
-//         productImageElement.style.filter = 'blur(3px)';
-//       }
-//     });
-//   })
-//   .catch(error => console.log(error));
-  
+      figcaptionElement.appendChild(buttonSalesElement);
+      productContainer.appendChild(figcaptionElement);
+      apiDataElement.appendChild(productContainer);
+    });
+  })
+  .catch(error => console.log(error));
